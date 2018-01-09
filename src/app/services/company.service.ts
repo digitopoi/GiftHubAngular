57d0a59c22b801513662224d3596af7e840cc972
digitopoi <hubo85@gmail.com>
2018-01-08 18:18:00 -0600

    Add getcompanies service method

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Company } from '../models/Company';
+
+const Api_Url = 'http://localhost:50203/Api';
 
 @Injectable()
 export class CompanyService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getCompanies() {
+    return this._http.get(`${Api_Url}/Company`, { headers: this.getHeaders()});
+  }
+
+  private getHeaders() {
+    return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+  }
 
 }
