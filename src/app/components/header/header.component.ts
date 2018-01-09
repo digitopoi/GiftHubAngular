c1aaa3a089646e01dc3e35f9c066b66f88e2acd4
Nick Bielawski <nbielawski12@gmail.com>
2018-01-09 11:05:04 -0500

    conditional nav buttons and welcome user message

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  username: string;
+  isLoggedIn: boolean;
 
-  constructor() { }
+  constructor(public router: Router, public _authService: AuthService) { }
 
   ngOnInit() {
+    this._authService.userInfo.subscribe((d: UserData) => {
+      console.log('the value of data', d);
+      this.username = d.user;
+      this.isLoggedIn = d.isloggedin;
+    });
   }
 
+  onLoggout() {
+    this._authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
+
+}
+
+export interface UserData {
+  user: string;
+  isloggedin: boolean;
 }
