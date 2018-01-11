38726aa530fd5f00a570b9269d679b58f5fe0b58
Carr OConnor <carr.oconnor@gmail.com>
2018-01-11 15:08:04 -0500

    total donations

--- src/app/components/total-donations/total-donations.component.ts
+++ src/app/components/total-donations/total-donations.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CardService } from '../../services/card.service';
+import { Card } from '../../models/Card';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-total-donations',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./total-donations.component.css']
 })
 export class TotalDonationsComponent implements OnInit {
+  card: Card[];
+  datasource: CardDataSource | null;
+  totalAmount: number = 0;
 
-  constructor() { }
+
+  constructor(private _cardService: CardService) { }
 
   ngOnInit() {
+    this._cardService.getDonation().subscribe((amount) => {
+      console.log(amount);
+      this.totalAmount = Number(amount);
+    });
+  }
+}
+
+export class CardDataSource extends DataSource<any> {
+  constructor (private cardData: Card[]) {
+    super();
   }
 
+  connect(): Observable<Card[]> {
+    return Observable.of(this.cardData);
+  }
+
+  disconnect() { }
 }
