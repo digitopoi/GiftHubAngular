47cd22817c82015efa5f122de7428b63b3bad0fd
digitopoi <hubo85@gmail.com>
2018-01-10 21:26:55 -0600

    Remove stray backslash

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Rx';
 import { HttpClient } from '@angular/common/http';
 
-const ApiUrl = 'http://localhost:50203//api/Account/adminCheck';
+const ApiUrl = 'http://localhost:50203/api/Account/adminCheck';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
@@ -13,7 +13,7 @@ export class AdminGuard implements CanActivate {
     constructor(private http: HttpClient, private router: Router) { }
 
     canActivate() {
-        return this.http.post(`${ApiUrl}`).map(isAdmin => {
+        return this.http.get(`${ApiUrl}`).map(isAdmin => {
             if (!isAdmin) {
                 this.router.navigate(['/user']);
                 return false;
