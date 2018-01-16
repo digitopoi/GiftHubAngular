33de4e78f08a901ae0c203e25bec08aa50c6f808
Beth Douglas <bethDdouglas@gmail.com>
2018-01-16 15:48:40 -0500

    added footer placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { TotalDonationsComponent } from './components/total-donations/total-dona
 import{StripeFormComponent} from './components/user/stripe/stripe-form.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { ImageComponent } from './components/landing/image/image.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 const routes = [
@@ -73,7 +74,8 @@ const routes = [
     TotalDonationsComponent,
     StripeFormComponent,
     LandingComponent,
-    ImageComponent
+    ImageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
